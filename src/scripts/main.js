a42de3051c018446332bb41c44dca4da712c42eb
Brandon Gray <bgizzle078@gmail.com>
2022-07-01 13:07:16 -0400

    first hairy potter push

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+// Imports go first
+import { makePottery } from './PotteryWheel.js'
+
+
+// Make 5 pieces of pottery at the wheel
+let mug = makePottery("Mug", "10 oz", "8 in");
+let plate = makePottery("Plate", "16 oz", "1 in")
+let bowl = makePottery("Bowl", "14 oz", "4 in")
+let cup = makePottery("Cup", "8 oz", "8 in")
+let platter = makePottery("Platter", "24 oz", "2 in")
+
+console.log(mug, plate, bowl, cup, platter)
+
+
+// Fire each piece of pottery in the kiln
+
+
+// Determine which ones should be sold, and their price
+
+
+// Invoke the component function that renders the HTML list
+
+
+
